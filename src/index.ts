f038655888b8566ac86a6e7d6c8d9b40db3cd694
cozmy <cosmin@hyperion.co>
2017-08-20 11:56:12 +0300

    Added a generic error handler

--- src/index.ts
+++ src/index.ts
@@ -15,5 +15,15 @@ app.use(databaseHandler);
 // Attach the routers
 app.use('/api', personsRouter);
 
+// Generic error handler
+app.use((err, req, res, next) => {
+  const status = err instanceof SyntaxError ? 400 : 500;
+
+  res.status(status).json({
+    message: err.message,
+    stack: err.stack
+  });
+});
+
 // Start the app at port 3000
 app.listen(3000);
