4d82b9e7eb0ccf663c37ace861c06ffd2b31ea06
cozmy <cosmin@hyperion.co>
2017-08-20 11:38:28 +0300

    The "Person" resource now has validation :O

--- src/person/index.ts
+++ src/person/index.ts
@@ -1,21 +1,89 @@
 import {Router} from 'express';
-import {personsPersistence} from './crud';
+import * as uniqid from 'uniqid';
 
-const personsRouter = Router();
+const personIdPrefix = 'person-';
+const personsRouter: Router = Router();
 
 personsRouter
-  .route('/persons/:id?')
+  .route('/persons/:id')
   .delete((req, res) => {
-    res.json(personsPersistence['delete'](res.locals.database, req.params.id));
+    const {database} = res.locals;
+    const {id} = req.params;
+    const person = database.get('persons').find({id}).value();
+
+    if (person && person.id === id) {
+      database.get('persons').remove({id}).write();
+
+      res.json(person);
+    } else {
+      res.status(404).send('404 Not Found - The requested "Person" was not found.');
+    }
   })
   .get((req, res) => {
-    res.json(personsPersistence.read(res.locals.database, req.params.id));
-  })
-  .post((req, res) => {
-    res.json(personsPersistence.create(res.locals.database, req.body));
+    const {database} = res.locals;
+    const {id} = req.params;
+    const person = database.get('persons').find({id}).value();
+
+    if (person && person.id === id) {
+      res.json(person);
+    } else {
+      res.status(404).send('404 Not Found - The requested "Person" was not found.');
+    }
   })
   .put((req, res) => {
-    res.json(personsPersistence.update(res.locals.database, req.params.id, req.body));
+    const {database} = res.locals;
+    const {id} = req.params;
+    const person = database.get('persons').find({id}).value();
+
+    if (person && person.id === id) {
+      const {firstName, lastName, money} = req.body;
+
+      if (typeof firstName === 'string' && firstName.length > 0) {
+        person.firstName = firstName;
+      }
+
+      if (typeof lastName === 'string' && lastName.length > 0) {
+        person.lastName = lastName;
+      }
+
+      if (typeof money === 'number') {
+        person.money = money;
+      }
+
+      database.get('persons').find({id}).assign(person).write();
+
+      res.json(person);
+    } else {
+      res.status(404).send('404 Not Found - The requested "Person" was not found.');
+    }
+  });
+
+personsRouter
+  .route('/persons/')
+  .get((req, res) => {
+    const {database} = res.locals;
+    const persons = database.get('persons');
+
+    res.json(persons);
+  })
+  .post((req, res) => {
+    const {firstName, lastName, money} = req.body;
+
+    if (
+      (typeof firstName === 'string' && firstName.length > 0) &&
+      (typeof lastName === 'string' && lastName.length > 0) &&
+      (typeof money === 'number')
+    ) {
+      const id = uniqid(personIdPrefix);
+      const person = {firstName, id, lastName, money};
+
+      const {database} = res.locals;
+      database.get('persons').push(person).write();
+
+      res.json(person);
+    } else {
+      res.status(400).send('400 Bad Request - Trying to create a "Person" with an incomplete payload.');
+    }
   });
 
 export {personsRouter};
